5c7b62c7088a9ae1bdb54caeef8d413ebbe406a0
yvesalexandrescott.dissakedissake <yvesalexandrescott.dissakedissake@et.esiea.fr>
2026-05-08 01:11:20 +0200

    chore(core): intégration de Sentry pour le monitoring des erreurs en production

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,30 @@
 # backend/app/main.py
 from fastapi import FastAPI
+import sentry_sdk
+import os
+from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth # On importe nos routes d'auth
 from app.api import threads  # <-- AJOUT : Import du nouveau routeur
 from app.models.database import init_db
 
 init_db()
+
+load_dotenv()
+
+# --- INITIALISATION DE SENTRY ---
+sentry_dsn = os.getenv("SENTRY_DSN")
+
+if sentry_dsn:
+    print(f"✅ Sentry DSN trouvé ! Sentry est activé.") # Un petit print pour nous rassurer
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        traces_sample_rate=1.0,
+        profiles_sample_rate=1.0,
+    )
+else:
+    print("❌ ATTENTION: Sentry DSN introuvable dans le .env !")
+
 
 app = FastAPI(title="CloseLoop API")
 
@@ -27,3 +46,4 @@ app.include_router(threads.router, prefix="/threads", tags=["Gmail Threads"])
 @app.get("/")
 def read_root():
     return {"message": "API Smart Follow-up opérationnelle 🚀"}
+
